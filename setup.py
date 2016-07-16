b8f5e3ff9aafed389d7ba888fbada1bb230770a4
apizzimenti <pizzimentianthony@gmail.com>
2016-07-16 03:14:44 -0500

    versioning

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ from distutils.core import setup
 setup(
     name="simple-engine-core",
     description="Reads in HTML, reads out JSON",
+    long_description=open("README.md").read(),
 
     license="MIT",
-    version=0.1,
+    version=0.2,
 
     url="https://github.com/apizzimenti/simple-engine-core.git",
     author="Anthony Pizzimenti",
     author_email="pizzimentianthony@gmail.com",
 
     packages=["core"],
+
     entry_points={
         "console_scripts": ["engine=core.index:main"],
     },
