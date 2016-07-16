4bbb61540c208ece7c889eb1018566e212369124
apizzimenti <pizzimentianthony@gmail.com>
2016-07-16 02:43:34 -0500

    add more stuff to config

--- core/interpreter/interpreter.py
+++ core/interpreter/interpreter.py
@@ -8,35 +8,34 @@ from core.tools import cvd, wtf
 
 class Interpreter:
 
-    def __init__(self, loc=os.getcwd(), name="index.html"):
+    def __init__(self, loc=os.getcwd()):
 
         self.loc = loc
-        self.name = name
-        self.path = p.join(loc, name)
-
         self.rawHtml = ""
 
-        self.options = {
-            "dest": "",
-            "filename": ""
-        }
+        self.options = {}
 
         self.DOM = ""
         self.JSON = ""
 
-    def config(self, configpath="config.json"):
+    def config(self):
         print("loading config")
-        with open(p.join(self.loc, configpath), "r") as f:
-            self.options = json.loads(f.read())
+
+        try:
+            with open(p.join(self.loc, "config.json"), "r") as f:
+                self.options = json.loads(f.read())
+        except:
+            raise FileNotFoundError("no config.json in this directory")
 
     def ingest(self):
-        self.rawHtml = bs(open(self.path), "html.parser")
+        path = p.join(self.loc, self.options["src"])
+        self.rawHtml = bs(open(path), "html.parser")
 
-    def structure(self, parentid="start"):
+    def structure(self):
         print("generating JSON")
-        self.DOM = cvd(self.rawHtml.find(id=parentid))
+        self.DOM = cvd(self.rawHtml.find(id=self.options["tophtmlid"]))
         self.JSON = json.dumps(self.DOM)
 
     def write(self):
         print("writing to {0}".format(self.options["dest"]))
-        wtf(self.options["dest"], self.JSON, filename="test")
+        wtf(self.options["cwd"], self.JSON, filename=self.options["dest"])
