616ea063f2497d0d1850637a0328cfbd715bb710
apizzimenti <pizzimentianthony@gmail.com>
2016-07-21 18:39:53 -0500

    only add children prop if children exist

--- core/tools/createvirtualdom.py
+++ core/tools/createvirtualdom.py
@@ -7,13 +7,13 @@ def createvirtualdom(element):
         "option": element["id"],
         "attrs": element.attrs,
         "type": element.name,
-        "text": element.contents[0].strip(),
-        "children": {}
+        "text": element.contents[0].strip()
     }
 
     children = element.find_all(True, recursive=False)
 
     if children:
+        obj["children"] = {}
         for child in children:
             obj["children"][child["id"]] = createvirtualdom(child)
 
